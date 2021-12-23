047caec8b712dd800db34c50278c70afdd2d4b8c
kingnan-Guo <373734199@qq.com>
2021-12-24 01:03:53 +0800

    新版本的 test

--- MCU/test/C/test.c
+++ MCU/test/C/test.c
@@ -3,15 +3,7 @@
 unsigned char T_IO[] = {"10101000"};
 
 
-void delay(unsigned int n)
-{
-	unsigned int i=0,j=0;
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<1200;j++);
-	}	
-		
-}
+
 
 int tenToTwo(num) {
     int i = 0, n, a[32];
@@ -23,11 +15,21 @@ int tenToTwo(num) {
         i = i + 1;
         n = n / 2;
     }
+    // i = 8;
     printf("十进制整数转换为二进制数是: %u  == ", num);
     for (i--; i >= 0; i--)
         printf("%d", a[i]);
     printf("\n");
     return 0;
+
+    // int i = 0, n, a[32];
+    // n = num;
+    // for (; n > 0; n /= 2) a[i++] = n % 2;
+    // printf("十进制整数转换为二进制数是:\n");
+    // for (i--; i >= 0; i--){
+    //     printf("%d", a[i]);
+    // } 
+
 }
 
 
@@ -36,25 +38,26 @@ int tenToTwo(num) {
 void computer(){
     // printf("%c", T_IO[0]);
     unsigned char readData = 0;
-    unsigned char i = 8;
+    unsigned char i = 9;
     for (i = 8; i > 0; i--){
         readData = readData >> 1;
-        delay(100);
+        // delay(100);
         // if(T_IO){
         //     readData |= 0x80; //没明白什么意思， 最高位移动到最低位，相当于一次还原？？？？？
 
         // }
         // printf("i = %u \n", i);
-        // printf("= %c \n", (T_IO[i]));
-        if(T_IO[i]=='1') {
+        // printf("T_IO == %c \n", T_IO[i]);
+        // printf("T_IO == %c \n", T_IO);
+        if(T_IO[i-1]=='1') {
             // printf("T_IO == %c \n", T_IO[i]);
             readData |= 0x80;
         }
         printf("readData = %u \n", readData);
         
         // printf("%c", T_IO[i]);
-        
         tenToTwo(readData);
+        
     }
     
 
