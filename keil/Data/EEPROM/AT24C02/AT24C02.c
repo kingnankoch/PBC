2144eae5b89fcd01d1cb772ba28abddf59b1854c
kingnan-Guo <373734199@qq.com>
2022-02-07 00:04:41 +0800

    尝试实现I2C 为内存写入数据

---
+++ keil/Data/EEPROM/AT24C02/AT24C02.c
@@ -0,0 +1,34 @@
+/**
+ * @file AT24C02.c
+ * @author kingnan (github.com/kingnan-Guo/PBC)
+ * @brief 可以电擦除的 内部储存器， 断电不消失;使用 I2C的方式进行数据存储
+ * @version 0.1
+ * @date 2022-02-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include"reg51.h"
+#include"intrins.h"
+// #include "stdio.h"
+
+// 初始化 AT24C02 
+void initAT24C02() {
+    _nop_()
+}
+
+void I2C_star() {
+
+}
+void I2C_stop() {
+
+}
+
+
+int main()
+{
+    
+    return 0;
+}
+
