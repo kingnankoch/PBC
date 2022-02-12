89caf3136d2dd66365478e0eb66ffd2f06037a75
kingnan-Guo <373734199@qq.com>
2022-02-12 15:42:54 +0800

    学习OLED 的Demo 文件，放在MCU中 ，以后学习代码的Demo也放在这里

---
+++ MCU/learningDemo/keil/OLED_I2c_C51/SRC/oled.c
@@ -0,0 +1,306 @@
+
+#include "oled.h"
+#include "oledfont.h"  	 
+//OLED���Դ�
+//��Ÿ�ʽ����?.
+//[0]0 1 2 3 ... 127	
+//[1]0 1 2 3 ... 127	
+//[2]0 1 2 3 ... 127	
+//[3]0 1 2 3 ... 127	
+//[4]0 1 2 3 ... 127	
+//[5]0 1 2 3 ... 127	
+//[6]0 1 2 3 ... 127	
+//[7]0 1 2 3 ... 127 			   
+void delay_ms(unsigned int ms)
+{                         
+	unsigned int a;
+	while(ms)
+	{
+		a=1800;
+		while(a--);
+		ms--;
+	}
+	return;
+}
+
+//���Ժ���
+void OLED_ColorTurn(u8 i)
+{
+	if(i==0)
+		{
+			OLED_WR_Byte(0xA6,OLED_CMD);//������ʾ
+		}
+	if(i==1)
+		{
+			OLED_WR_Byte(0xA7,OLED_CMD);//��ɫ��ʾ
+		}
+}
+
+//��Ļ��ת180��
+void OLED_DisplayTurn(u8 i)
+{
+	if(i==0)
+		{
+			OLED_WR_Byte(0xC8,OLED_CMD);//������ʾ
+			OLED_WR_Byte(0xA1,OLED_CMD);
+		}
+	if(i==1)
+		{
+			OLED_WR_Byte(0xC0,OLED_CMD);//��ת��ʾ
+			OLED_WR_Byte(0xA0,OLED_CMD);
+		}
+}
+
+
+//��ʱ
+void IIC_delay(void)
+{
+	u8 t=1;
+	while(t--);
+}
+
+//��ʼ�ź�
+void I2C_Start(void)
+{
+	OLED_SDA_Set();
+	OLED_SCL_Set();
+	IIC_delay();
+	OLED_SDA_Clr();
+	IIC_delay();
+	OLED_SCL_Clr();
+	 
+}
+
+//�����ź�
+void I2C_Stop(void)
+{
+	OLED_SDA_Clr();
+	OLED_SCL_Set();
+	IIC_delay();
+	OLED_SDA_Set();
+}
+
+//�ȴ��ź���Ӧ
+void I2C_WaitAck(void) //�������źŵĵ�ƽ
+{
+	OLED_SDA_Set();
+	IIC_delay();
+	OLED_SCL_Set();
+	IIC_delay();
+	OLED_SCL_Clr();
+	IIC_delay();
+}
+
+//д��һ���ֽ�
+void Send_Byte(u8 dat)
+{
+	u8 i;
+	for(i=0;i<8;i++)
+	{
+		OLED_SCL_Clr();//��ʱ���ź�����Ϊ�͵�ƽ
+		if(dat&0x80)//��dat��8λ�����λ����д��?
+		{
+			OLED_SDA_Set();
+    }
+		else
+		{
+			OLED_SDA_Clr();
+    }
+		IIC_delay();
+		OLED_SCL_Set();
+		IIC_delay();
+		OLED_SCL_Clr();
+		dat<<=1;
+  }
+}
+
+//����һ���ֽ�
+//��SSD1306д��һ���ֽڡ�
+//mode:����/������? 0,��ʾ����;1,��ʾ����;
+void OLED_WR_Byte(u8 dat,u8 mode)
+{
+	I2C_Start();
+	Send_Byte(0x78);
+	I2C_WaitAck();
+	if(mode){Send_Byte(0x40);}
+  else{Send_Byte(0x00);}
+	I2C_WaitAck();
+	Send_Byte(dat);
+	I2C_WaitAck();
+	I2C_Stop();
+}
+
+//��������
+
+void OLED_Set_Pos(u8 x, u8 y) 
+{ 
+	OLED_WR_Byte(0xb0+y,OLED_CMD);
+	OLED_WR_Byte(((x&0xf0)>>4)|0x10,OLED_CMD);
+	OLED_WR_Byte((x&0x0f),OLED_CMD);
+}   	  
+//����OLED��ʾ    
+void OLED_Display_On(void)
+{
+	OLED_WR_Byte(0X8D,OLED_CMD);  //SET DCDC����
+	OLED_WR_Byte(0X14,OLED_CMD);  //DCDC ON
+	OLED_WR_Byte(0XAF,OLED_CMD);  //DISPLAY ON
+}
+//�ر�OLED��ʾ     
+void OLED_Display_Off(void)
+{
+	OLED_WR_Byte(0X8D,OLED_CMD);  //SET DCDC����
+	OLED_WR_Byte(0X10,OLED_CMD);  //DCDC OFF
+	OLED_WR_Byte(0XAE,OLED_CMD);  //DISPLAY OFF
+}		   			 
+//��������,������,������Ļ�Ǻ�ɫ��!��û����һ��!!!	  
+void OLED_Clear(void)  
+{  
+	u8 i,n;		    
+	for(i=0;i<8;i++)  
+	{  
+		OLED_WR_Byte (0xb0+i,OLED_CMD);    //����ҳ��ַ��0~7��
+		OLED_WR_Byte (0x00,OLED_CMD);      //������ʾλ�á��е͵�ַ
+		OLED_WR_Byte (0x10,OLED_CMD);      //������ʾλ�á��иߵ�ַ   
+		for(n=0;n<128;n++)OLED_WR_Byte(0,OLED_DATA); 
+	} //������ʾ
+}
+
+//��ָ��λ����ʾһ���ַ�,���������ַ�
+//x:0~127
+//y:0~63				 
+//sizey:ѡ������ 6x8  8x16
+void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 sizey)
+{      	
+	u8 c=0,sizex=sizey/2;
+	u16 i=0,size1;
+	if(sizey==8)size1=6;
+	else size1=(sizey/8+((sizey%8)?1:0))*(sizey/2);
+	c=chr-' ';//�õ�ƫ�ƺ���?
+	OLED_Set_Pos(x,y);
+	for(i=0;i<size1;i++)
+	{
+		if(i%sizex==0&&sizey!=8) OLED_Set_Pos(x,y++);
+		if(sizey==8) OLED_WR_Byte(asc2_0806[c][i],OLED_DATA);//6X8�ֺ�
+		else if(sizey==16) OLED_WR_Byte(asc2_1608[c][i],OLED_DATA);//8x16�ֺ�
+//		else if(sizey==xx) OLED_WR_Byte(asc2_xxxx[c][i],OLED_DATA);//�û������ֺ�
+		else return;
+	}
+}
+//m^n����
+u32 oled_pow(u8 m,u8 n)
+{
+	u32 result=1;	 
+	while(n--)result*=m;    
+	return result;
+}				  
+//��ʾ����
+//x,y :�������?
+//num:Ҫ��ʾ������
+//len :���ֵ�λ��
+//sizey:������?		  
+void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 sizey)
+{         	
+	u8 t,temp,m=0;
+	u8 enshow=0;
+	if(sizey==8)m=2;
+	for(t=0;t<len;t++)
+	{
+		temp=(num/oled_pow(10,len-t-1))%10;
+		if(enshow==0&&t<(len-1))
+		{
+			if(temp==0)
+			{
+				OLED_ShowChar(x+(sizey/2+m)*t,y,' ',sizey);
+				continue;
+			}else enshow=1;
+		}
+	 	OLED_ShowChar(x+(sizey/2+m)*t,y,temp+'0',sizey);
+	}
+}
+//��ʾһ���ַ��Ŵ�
+void OLED_ShowString(u8 x,u8 y,u8 *chr,u8 sizey)
+{
+	u8 j=0;
+	while (chr[j]!='\0')
+	{		
+		OLED_ShowChar(x,y,chr[j++],sizey);
+		if(sizey==8)x+=6;
+		else x+=sizey/2;
+	}
+}
+//��ʾ����
+void OLED_ShowChinese(u8 x,u8 y,u8 no,u8 sizey)
+{
+	u16 i,size1=(sizey/8+((sizey%8)?1:0))*sizey;
+	for(i=0;i<size1;i++)
+	{
+		if(i%sizey==0) OLED_Set_Pos(x,y++);
+		if(sizey==16) OLED_WR_Byte(Hzk[no][i],OLED_DATA);//16x16�ֺ�
+//		else if(sizey==xx) OLED_WR_Byte(xxx[c][i],OLED_DATA);//�û������ֺ�
+		else return;
+	}				
+}
+
+
+//��ʾͼƬ
+//x,y��ʾ����
+//sizex,sizey,ͼƬ����
+//BMP��Ҫ��ʾ��ͼƬ
+void OLED_DrawBMP(u8 x,u8 y,u8 sizex, u8 sizey,u8 BMP[])
+{ 	
+  u16 j=0;
+	u8 i,m;
+	sizey=sizey/8+((sizey%8)?1:0);
+	for(i=0;i<sizey;i++)
+	{
+		OLED_Set_Pos(x,i+y);
+    for(m=0;m<sizex;m++)
+		{      
+			OLED_WR_Byte(BMP[j++],OLED_DATA);	    	
+		}
+	}
+} 
+
+
+
+//��ʼ��				    
+void OLED_Init(void)
+{
+// 	OLED_RES_Clr();
+//   delay_ms(200);
+// 	OLED_RES_Set();
+	
+	OLED_WR_Byte(0xAE,OLED_CMD);//--turn off oled panel
+	OLED_WR_Byte(0x00,OLED_CMD);//---set low column address
+	OLED_WR_Byte(0x10,OLED_CMD);//---set high column address
+	OLED_WR_Byte(0x40,OLED_CMD);//--set start line address  Set Mapping RAM Display Start Line (0x00~0x3F)
+	OLED_WR_Byte(0x81,OLED_CMD);//--set contrast control register
+	OLED_WR_Byte(0xCF,OLED_CMD); // Set SEG Output Current Brightness
+	OLED_WR_Byte(0xA1,OLED_CMD);//--Set SEG/Column Mapping     0xa0���ҷ��� 0xa1����
+	OLED_WR_Byte(0xC8,OLED_CMD);//Set COM/Row Scan Direction   0xc0���·��� 0xc8����
+	OLED_WR_Byte(0xA6,OLED_CMD);//--set normal display
+	OLED_WR_Byte(0xA8,OLED_CMD);//--set multiplex ratio(1 to 64)
+	OLED_WR_Byte(0x3f,OLED_CMD);//--1/64 duty
+	OLED_WR_Byte(0xD3,OLED_CMD);//-set display offset	Shift Mapping RAM Counter (0x00~0x3F)
+	OLED_WR_Byte(0x00,OLED_CMD);//-not offset
+	OLED_WR_Byte(0xd5,OLED_CMD);//--set display clock divide ratio/oscillator frequency
+	OLED_WR_Byte(0x80,OLED_CMD);//--set divide ratio, Set Clock as 100 Frames/Sec
+	OLED_WR_Byte(0xD9,OLED_CMD);//--set pre-charge period
+	OLED_WR_Byte(0xF1,OLED_CMD);//Set Pre-Charge as 15 Clocks & Discharge as 1 Clock
+	OLED_WR_Byte(0xDA,OLED_CMD);//--set com pins hardware configuration
+	OLED_WR_Byte(0x12,OLED_CMD);
+	OLED_WR_Byte(0xDB,OLED_CMD);//--set vcomh
+	OLED_WR_Byte(0x40,OLED_CMD);//Set VCOM Deselect Level
+	OLED_WR_Byte(0x20,OLED_CMD);//-Set Page Addressing Mode (0x00/0x01/0x02)
+	OLED_WR_Byte(0x02,OLED_CMD);//
+	OLED_WR_Byte(0x8D,OLED_CMD);//--set Charge Pump enable/disable
+	OLED_WR_Byte(0x14,OLED_CMD);//--set(0x10) disable
+	OLED_WR_Byte(0xA4,OLED_CMD);// Disable Entire Display On (0xa4/0xa5)
+	OLED_WR_Byte(0xA6,OLED_CMD);// Disable Inverse Display On (0xa6/a7) 
+	OLED_Clear();
+	OLED_WR_Byte(0xAF,OLED_CMD); /*display ON*/ 
+}
+
+
+
+
