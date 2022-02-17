2d75f892cb207fe9d6a4038a938db96d536d71b2
kingnan-Guo <373734199@qq.com>
2022-02-18 00:55:55 +0800

    创建串口调试打印page

---
+++ keil/Data/STC_IPS_Page/STC_IPS_FIRST/STC_IPS.c
@@ -0,0 +1,11 @@
+/**
+ * @file STC_IPS.c
+ * @author kingnan (github.com/kingnan-Guo/PBC)
+ * @brief STC-IPS 串口调试
+ * @version 0.1
+ * @date 2022-02-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include ""#include "reg51.h"
