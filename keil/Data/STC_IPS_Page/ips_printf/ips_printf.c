f889f7c6f824b3888ed15df221bd17e7300ac03e
kingnan-Guo <373734199@qq.com>
2022-03-10 00:16:27 +0800

    串口发送一串数据及printf重定向 项目创建

---
+++ keil/Data/STC_IPS_Page/ips_printf/ips_printf.c
@@ -0,0 +1,13 @@
+/**
+ * @file ips_printf.c
+ * @author kingnan (github.com/kingnan-Guo/PBC)
+ * @brief 串口发送一串数据及printf重定向
+ * @version 0.1
+ * @date 2022-03-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "reg51.h"
+
+
