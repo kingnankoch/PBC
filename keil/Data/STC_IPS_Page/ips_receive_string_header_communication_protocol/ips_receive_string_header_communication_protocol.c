dbb7caec84e3120f90ca4c8d216022e33fed0721
kingnan-Guo <373734199@qq.com>
2022-04-06 17:37:19 +0800

    创建项目 判断数据帧头来接收哟串数据的串口通信

---
+++ keil/Data/STC_IPS_Page/ips_receive_string_header_communication_protocol/ips_receive_string_header_communication_protocol.c
@@ -0,0 +1,16 @@
+/**
+ * @file ips_receive_string_header_communication_protocol.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+int main()
+{
+    /* code */
+    return 0;
+}
+
---
+++ keil/Data/STC_IPS_Page/ips_receive_string_header_communication_protocol/uart.c
@@ -0,0 +1,14 @@
+/**
+ * @file uart.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-06
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "reg51.h"
+#include "uart.h"
+
